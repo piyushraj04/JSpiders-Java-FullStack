1fd6b6f07f6d1d2f25e50cbd48304c1dd8df3027
Piyush Raj <piyush004.raj@gmail.com>
2026-02-26 01:26:42 +0530

    FizzBuzz

---
+++ JavaScript/Basics/FizzBuzz.js
@@ -0,0 +1,14 @@
+let n = 15
+if(n%3==0 && n%5==0){
+    console.log("FizzBuzz");
+}
+else if(n%3==0){
+    console.log("Fizz");
+}
+else if(n%5==0){
+    console.log("Buzz");
+}
+else{
+    console.log("Not divisible by 3 or 5");
+    
+}
