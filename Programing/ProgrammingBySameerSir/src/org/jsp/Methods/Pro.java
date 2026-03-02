0174e4cf8e2a379a593847761a744b204761a3c6
Piyush Raj <piyush004.raj@gmail.com>
2026-03-02 22:45:14 +0530

    Pyramid pallindrom DSA

---
+++ Programing/ProgrammingBySameerSir/src/org/jsp/Methods/Pro.java
@@ -0,0 +1,21 @@
+package org.jsp.Methods;
+
+public class Pro {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n-i; j++){
+                System.out.print(" "+" ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print(j+" ");
+            }
+            for(int j = 1; j < i; j++){
+                System.out.print((i-j)+" ");
+            }
+            System.out.println();
+            
+        }
+    }
+    
+}
